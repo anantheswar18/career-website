8843ad0b51deb0d19267e269fd1da8d517ed1a9a
anantheswar18 <119279855+anantheswar18@users.noreply.github.com>
2025-01-02 11:18:19 +0000

    Jobitem and json created
    Dynamic templete created

--- app.py
+++ app.py
@@ -1,15 +1,46 @@
-from flask import Flask
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        "id":1,
+        "name" : "Deepak Prabhu",
+        "job title" : "Database Architect",
+        "location" : "Kochi,Kerala"
+    },
+    {
+        "id":2,
+        "name" : "Titto Bobby",
+        "job title" : "Database Developer",
+        "location" : "Kochi,Kerala"
+    },
+    {
+        "id":3,
+        "name" : "Vivek",
+        "job title" : "Quality Analyst",
+        "location" : "Kochi,Kerala"
+    },
+    {
+        "id":4,
+        "name" : "Sulekha Naidu",
+        "job title" : "Backend Senior Developer",
+        "location" : "Bangalore,Kerala"
+    },
+]
+
 @app.route("/home")
 def home():
-    return "Hello, I am Anantheswar"
+    return render_template("home.html")
 
-@app.route("/friend")
+@app.route("/carrer")
 def Second():
-    return "Neee verum M@app.route"
+    return render_template("proHome.html",jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 @app.route("/sreeja")
 def forSreeja():
@@ -23,5 +54,8 @@ def forReema():
 @app.route("/sadhika")
 def forsadhika():
     return "Hey Sadhika Chechi, You are sooo good and sooo caring we love you sooo much espeaciallly me.Keep going like this. You are always in my prayers God bless you.Merry Christmas and Happy New Year 🥳🎅🍾😘🎇"
+
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
